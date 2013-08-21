e68358b94f46812672a1a09fa27269c6da82850e
Adam Sax <adam@spotlabs.com>
2013-08-21 10:45:27 -0700

    new examples for keyboard and idle

--- src/com/spotlabs/example/MainActivity.java
+++ src/com/spotlabs/example/MainActivity.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2013 Spot Labs Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.spotlabs.example;
 
 import java.util.ArrayList;
@@ -6,6 +21,7 @@ import java.util.List;
 
 import com.spotlabs.example.behavior.ActivateIdle;
 import com.spotlabs.example.cache.SimpleCache;
+import com.spotlabs.example.keyboard.KeyboardTypes;
 import com.spotlabs.example.messaging.FormMessage;
 
 import android.os.Bundle;
@@ -29,6 +45,7 @@ public class MainActivity extends Activity implements OnItemClickListener {
 			add(new MainActivity.Example("Simple Caching", SimpleCache.class));
 			add(new MainActivity.Example("Simple Form Message", FormMessage.class));
 			add(new MainActivity.Example("Force Idle", ActivateIdle.class));
+			add(new MainActivity.Example("Keyboard Types", KeyboardTypes.class));
 	}};
 	
 	private ArrayAdapter<Example> mAdapter;
---
+++ src/com/spotlabs/example/keyboard/KeyboardTypes.java
@@ -0,0 +1,15 @@
+package com.spotlabs.example.keyboard;
+
+import com.spotlabs.example.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class KeyboardTypes extends Fragment {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.example_keyboard_types, container, false);
+	}
+}
