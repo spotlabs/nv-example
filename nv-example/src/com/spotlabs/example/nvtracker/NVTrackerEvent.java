fa905bc16a51502044e63d6820390d7508c2b460
Jeff Hollar <jhollar2006@gmail.com>
2013-09-13 08:04:36 -0700

    Updated the nvtracker api example to reflect changes made in both nv-core and nv-platform

--- nv-example/src/com/spotlabs/example/nvtracker/NVTrackerEvent.java
+++ nv-example/src/com/spotlabs/example/nvtracker/NVTrackerEvent.java
@@ -19,16 +19,29 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.spotlabs.nvlogcattest.NSLogcatTest;
+import com.spotlabs.nvtracker.api.INVTrackerShadowService;
+import com.spotlabs.nvtracker.api.NVTrackerAPIFactory;
+import com.spotlabs.nvtracker.api.NVTrackerEventApi;
+import com.spotlabs.nvtracker.api.PiwikMessageRequest;
 import com.spotlabs.nvtracker.common.NVApiMethod;
 import com.spotlabs.nvtracker.common.NVSites;
 import com.spotlabs.nvtrackerdb.NVLogRecordState;
 import com.spotlabs.nvtrackerdb.NVTrackerLogRecord;
+import com.spotlabs.nvtrackershadow.NVTrackerShadowService;
 
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 
 public class NVTrackerEvent extends Fragment {
+
+	INVTrackerShadowService mINVTrackerShadowService;
+	boolean cBound = false;
 
 	/* (non-Javadoc)
 	 * @see android.app.Fragment#onActivityCreated(android.os.Bundle)
@@ -37,39 +50,69 @@ public class NVTrackerEvent extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onActivityCreated(savedInstanceState);
-
-		NVTrackerEventApi eventApi = NVTrackerAPIFactory.getEventAPI(getApplicationContext());
-
-		// The following provides three ways of tracking events:
-		//	- single event message.
-		//  - single event message object 
-		//  - a list event message objects. 
-		//
-
+	
+		// Obtain the unique location identifier for this device. 					
+		NVTrackerEventApi eventApi = NVTrackerAPIFactory.getEventAPI(mINVTrackerShadowService);
+	
 		// ---
 		// Log a single event by type and detail
 		eventApi.logEvent("test01", "Something about this event goes here");
 
 		// ---
 		// Log a single event using an event log record object
-		NVTrackerLogRecord logRecord = new NVTrackerLogRecord();
-		logRecord.setEvent_type("test02");
-		logRecord.setDetails("event details goes here");
+		PiwikMessageRequest logRecord =
+				new PiwikMessageRequest( NVSites.NVEVENT.getValue(), NVApiMethod.ADD.getValue(),
+						"nvevent-test02", "event details goes here", new Date().getTime());	
 		eventApi.logEvent(logRecord);
 
 		// ---
 		// Log a list of event objects
-		List<NVTrackerLogRecord> logRecords = new ArrayList<NVTrackerLogRecord>();
+		List<PiwikMessageRequest> logRecords = new ArrayList<PiwikMessageRequest>();
 
 		// Create two log record objects 
 		for (int i=0; i< 2; i++) {
-			NVTrackerLogRecord logRecord = new NVTrackerLogRecord();
-			logRecord.setEvent_type("test"+i);
-			logRecord.setDetails("event details goes here");
+			PiwikMessageRequest logRecord =
+					new PiwikMessageRequest( NVSites.NVEVENT.getValue(), NVApiMethod.ADD.getValue(),
+							"test"+i, "event details goes here", new Date().getTime());	
 			logRecords.add(logRecord);
 		}
 		eventApi.logEvents(logRecords);
 
 	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Fragment#onStart()
+	 */
+	@Override
+	public void onStart() {
+		super.onStart();
+		bindService(new Intent(NVTrackerEvent.this, NVTrackerShadowService.class), mConnection, BIND_AUTO_CREATE);
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Fragment#onStop()
+	 */
+	@Override
+	public void onStop() {
+		super.onStop();
+		if(cBound) {
+			unbindService(mConnection);
+			cBound = false; 
+		}
+	}
+
+	ServiceConnection mConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mINVTrackerShadowService = INVTrackerShadowService.Stub.asInterface(service);
+			cBound = true;
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mINVTrackerShadowService = null;
+			cBound = false;
+		}
+	};
 
 }
